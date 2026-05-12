330753a386efc95ef94bca2a3c1005c516b3e991
sharmasuraj0123 <sharmasuraj0123@gmail.com>
2026-05-12 19:01:45 +0530

    Initial version of Cowork APIdocs:

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 import { createMDX } from "fumadocs-mdx/next";
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  turbopack: {
+    root: dirname(fileURLToPath(import.meta.url)),
+  },
 };
 
 export default withMDX(config);
